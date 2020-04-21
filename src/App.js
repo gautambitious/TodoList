e0c82a96a3e5728a496dbea5419dcd4f3f1c9042
gautambitious <mvgt.gautam@gmail.com>
2020-04-21 17:00:17 +0530

    Sidebad | Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Header} from './components/layouts/Header';
+import {Content} from "./components/layouts/Content";
 
 export const App = () => {
   return (
     <div className="App">
-		<Header /> 
+		<Header />
+		<Content/>
     </div>
   );
 }
---
+++ src/components/layouts/Content.js
@@ -0,0 +1,6 @@
+import React from "react";
+import {Sidebar} from "./Sidebar";
+
+export const Content = () => {
+    return <Sidebar/>
+}
