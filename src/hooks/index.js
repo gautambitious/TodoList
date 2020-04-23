05285911b806070fcf581807cfb87e8ed399637f
gautambitious <mvgt.gautam@gmail.com>
2020-04-23 16:06:33 +0530

    Custom Hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -43,7 +43,22 @@ export const useProjects = () => {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        firebase.firestore().collection('projects').where('userId', '==', 'dsdsda')
+        firebase
+            .firestore()
+            .collection('projects')
+            .where('userId', '==', 'dsdsda')
+            .orderBy('projectId')
+            .get()
+            .then(snapshot =>{
+                const allProjects = snapshot.docs.map(project => ({
+                    ...project.data(),
+                    docId: project.id
+                }));
 
-    }, []);
-}
+                if(JSON.stringify(allProjects) !== JSON.stringify(projects)){
+                    setProjects(allProjects)
+                }
+            })
+    }, [projects]);
+    return {projects, setProjects};
+};
